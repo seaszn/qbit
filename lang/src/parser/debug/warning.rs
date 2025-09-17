778edf05e8470c9c613e47cc022861402f8bd30e
Seaszn <111234706+seaszn@users.noreply.github.com>
2025-09-17 15:18:09 +0200

    adding unused variable warning

--- lang/src/parser/debug/warning.rs
+++ lang/src/parser/debug/warning.rs
@@ -1,24 +1,24 @@
 // lang/src/parser/warning.rs
 
+use super::DebugContext;
 use std::ops::Range;
 use thiserror::Error;
-use super::DebugContext;
 
 #[derive(Debug, Clone, Error)]
 pub enum ParseWarning {
-    // /// Variable declared but never used
-    // UnusedVariable {
-    //     name: String,
-    //     span: Range<usize>,
-    //     context: DebugContext,
-    // },
+    /// Variable declared but never used
+    UnusedVariable {
+        name: String,
+        span: Range<usize>,
+        context: DebugContext,
+    },
 
-    // /// Function declared but never used
-    // UnusedFunction {
-    //     name: String,
-    //     span: Range<usize>,
-    //     context: DebugContext,
-    // },
+    /// Function declared but never used
+    UnusedFunction {
+        name: String,
+        span: Range<usize>,
+        context: DebugContext,
+    },
 
     /// Code after return statement
     UnreachableCode {
@@ -37,22 +37,24 @@ pub enum ParseWarning {
 impl std::fmt::Display for ParseWarning {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            // ParseWarning::UnusedVariable { name, context, .. } => {
-            //     write!(f, "Variable '{}' is declared but never used", name)?;
-            //     write!(f, "\n{}", context)?;
-            //     Ok(())
-            // }
-            // ParseWarning::UnusedFunction { name, context, .. } => {
-            //     write!(f, "Function '{}' is declared but never used", name)?;
-            //     write!(f, "\n{}", context)?;
-            //     Ok(())
-            // }
+            ParseWarning::UnusedVariable { name, context, .. } => {
+                write!(f, "Variable '{}' is declared but never used", name)?;
+                write!(f, "\n{}", context)?;
+                Ok(())
+            }
+            ParseWarning::UnusedFunction { name, context, .. } => {
+                write!(f, "Function '{}' is declared but never used", name)?;
+                write!(f, "\n{}", context)?;
+                Ok(())
+            }
             ParseWarning::UnreachableCode { context, .. } => {
                 write!(f, "Unreachable code")?;
                 write!(f, "\n{}", context)?;
                 Ok(())
             }
-            ParseWarning::NamingConvention { message, context, .. } => {
+            ParseWarning::NamingConvention {
+                message, context, ..
+            } => {
                 write!(f, "{}", message)?;
                 write!(f, "\n{}", context)?;
                 Ok(())
